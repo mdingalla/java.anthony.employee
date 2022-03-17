d8e3dfe8faa5659604e3dcabe40adef012977c95
antots01 <keystoneshop8@gmail.com>
2022-03-17 23:15:03 +0800

    Updated

--- RunEmployee.java
+++ RunEmployee.java
@@ -13,7 +13,7 @@ public class RunEmployee{
 	        System.out.print("Employee name: ");
 	        String emp_name =input.nextLine();
 
-			boolean isNumber;
+			boolean isNumber = false;
 			
 	        
 			do{
@@ -21,63 +21,64 @@ public class RunEmployee{
 				if(!input.hasNextInt()){ //not a number
 					// number = input.nextLine();
 					isNumber= false;
-				}else{
-					System.out.println("ERROR! please try again");
-					isNumber = true;
-					input.next();
-				}
+					char job_criteria =input.next().charAt(0);
+	        
+					char select = Character.toUpperCase(job_criteria);
+					
+					
+					Employee emp;
+					
+					if (select == 'F') {
+						System.out.println("--- Full Time Employee ---");
+						System.out.print("Enter Basic Pay:  ");
+						double basic_pay = input.nextDouble();
+		
+						
+		
+						emp = new FullTimeEmployee(emp_name,basic_pay);
+						// System.out.println("Basic Pay " + basic_pay);
+		
+						System.out.println("____________________________________");
+						System.out.println("Employee Name:   " + emp_name );
+						System.out.println("Basic Pay:       " + df2.format(basic_pay));
+						
+						System.out.println("____________________________________");
+						System.out.println("Monthly Salary:       " + df2.format(emp.monthSal()));
+						System.out.println("");
+					} else if (select == 'P') {
+						
+						System.out.println("--- Part Time Employee ---");
+						System.out.print("Enter rate per hour:  ");
+						double rate_per_hour = input.nextDouble();
+						
+						System.out.print("Enter no. of hours worked:  ");
+						int no_hours_work2 = input.nextInt();
+		
+					   
+						double basic_pay2 =  (rate_per_hour * no_hours_work2);
+						 
+					   
+						PartTimeEmployee pte = new PartTimeEmployee(emp_name,no_hours_work2,rate_per_hour,0);
+		
+						System.out.println("___________________________________");
+						System.out.println("Employee Name:   " + pte.getEmp_name() );
+						System.out.println("Monthly Wage:       " + df2.format(basic_pay2));
+						System.out.println("___________________________________");
+						System.out.println("Deductions:" + df2.format(pte.getdeduction()));
+						System.out.println("Wage:       " + df2.format(pte.wage()));
+						System.out.println("");
+					}
+					else{
+						System.out.println("ERROR! please try again");
+					    isNumber = true;
+					    //input.next();
+					}
+				}	 
+				
 			} while((isNumber));
 			// System.out.println(number);
-	        char job_criteria =input.next().charAt(0);
-	        
-	        char select = Character.toUpperCase(job_criteria);
-	        
-	        
-	        Employee emp;
-	        
-	        if (select == 'F') {
-	        	System.out.println("--- Full Time Employee ---");
-	        	System.out.print("Enter Basic Pay:  ");
-	            double basic_pay = input.nextDouble();
+        }
+	}	        
 
-				
-
-                emp = new FullTimeEmployee(emp_name,basic_pay);
-	            // System.out.println("Basic Pay " + basic_pay);
-
-	            System.out.println("____________________________________");
-	            System.out.println("Employee Name:   " + emp_name );
-	            System.out.println("Basic Pay:       " + df2.format(basic_pay));
-                
-	            System.out.println("____________________________________");
-	            System.out.println("Monthly Salary:       " + df2.format(emp.monthSal()));
-	            System.out.println("");
-	        } else if (select == 'P') {
-	        	
-	        	System.out.println("--- Part Time Employee ---");
-	        	System.out.print("Enter rate per hour:  ");
-	            double rate_per_hour = input.nextDouble();
-	            
-	            System.out.print("Enter no. of hours worked:  ");
-	            int no_hours_work2 = input.nextInt();
-
-               
-	            double basic_pay2 =  (rate_per_hour * no_hours_work2);
-				 
-	           
-                PartTimeEmployee pte = new PartTimeEmployee(emp_name,no_hours_work2,rate_per_hour,0);
-
-	            System.out.println("___________________________________");
-	            System.out.println("Employee Name:   " + pte.getEmp_name() );
-	            System.out.println("Monthly Wage:       " + df2.format(basic_pay2));
-	            System.out.println("___________________________________");
-				System.out.println("Deductions:" + df2.format(pte.getdeduction()));
-				System.out.println("Wage:       " + df2.format(pte.wage()));
-				System.out.println("");
-	        } 
-        }	        
-
-
-    }
-
-} 
+}
+ 
